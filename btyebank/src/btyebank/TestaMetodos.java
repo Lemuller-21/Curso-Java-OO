751b018602bb48136dd818f4f2fb2defbfd86d47
leona <leona@192.168.1.14>
2021-07-20 20:53:15 -0300

    Atualizei a conta e criei o arquivo "Cliente"

--- btyebank/src/btyebank/TestaMetodos.java
+++ btyebank/src/btyebank/TestaMetodos.java
@@ -5,21 +5,11 @@ public class TestaMetodos {
 	public static void main(String[] args) {
 
 		Conta conta1 = new Conta();
-		System.out.println(conta1.saldo);
+		conta1.titular = new Cliente();
 		
-		conta1.deposita(150);
-		System.out.println(conta1.saldo);
+		conta1.titular.nome = "ayayayayayayayayaayayayayayayaya";
 		
-		conta1.saca(50);
-		System.out.println(conta1.saldo);
-		
-		Conta conta2 = new Conta();
-		conta2.deposita(400);
-		
-		System.out.println(conta2.transfere(150, conta1));
-		
-		System.out.println(conta1.saldo);
-		System.out.println(conta2.saldo);
+		System.out.println(conta1.titular.nome);
 	}
 
 }
